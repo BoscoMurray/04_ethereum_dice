28145b19bd8e49319230d9b8285ae1479757a2ca
BoscoMurray <rossamurray@gmail.com>
2017-09-04 13:05:33 +0100

    can set value in Dice contract

--- src/containers/Dice.jsx
+++ src/containers/Dice.jsx
@@ -9,46 +9,83 @@ class Dice extends React.Component {
     super(props);
 
     this.state = {
-      web3: null
+      web3: null,
+      contract: null,
       lastRoll: 0,
-      playerFunds: 0,
-      houseFunds: 0
+      playerAddress: "0x39ba05291564d3f184c8ec24042f19a13b5b7d72",
+      playerBalance: 0,
+      houseAddress: "0x90510cec7e77d80cd6f60804b3ca556935cbcc8d",
+      houseBalance: 0
     };
+    this.setLastRoll = this.setLastRoll.bind(this);
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
     getWeb3
     .then(results => {
       this.setState({
         web3: results.web3
       })
-
-      // Instantiate contract once web3 provided.
       this.instantiateContract()
+      this.getBalances()
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
+    .then(() => {
+      const tempWeb3 = this.state.web3;
+      tempWeb3.eth.defaultAccount = "0x68aa944b28982761c753e3b10d6a3d435e0f27c7"
+      this.setState({ web3: tempWeb3 })
+    })
+
   }
 
   instantiateContract() {
-      const contract = require('truffle-contract')
-      const dice = contract(DiceContract)
-      dice.setProvider(this.state.web3.currentProvider)
+    const contract = require('truffle-contract')
+    const dice = contract(DiceContract)
+    dice.setProvider(this.state.web3.currentProvider)
 
-      var diceInstance;
+    var diceInstance;
 
-      this.state.web3.eth.getAccounts((error, accounts) => {
-        dice.deployed().then((instance) => {
-          diceInstance = instance
-          return diceInstance.getLastRoll.call(accounts[0])
-        }).then((result) => {
-          return this.setState({ lastRoll: result.c[0] })
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      dice.deployed().then((instance) => {
+        diceInstance = instance
+        this.setState({ contract: diceInstance })
+        return diceInstance.getLastRoll.call(accounts[0])
+      }).then((result) => {
+        return this.setState({ lastRoll: result.c[0] })
       });
-    }
+    })
+
+  }
+
+  getBalances() {
+    this.state.web3.eth.getBalance(this.state.playerAddress, function(error, result){
+        if(!error)
+            return this.setState({ playerBalance: result.c[0] });
+        else
+            console.error(error);
+    }.bind(this))
+
+    this.state.web3.eth.getBalance(this.state.houseAddress, function(error, result){
+        if(!error)
+            return this.setState({ houseBalance: result.c[0] });
+        else
+            console.error(error);
+    }.bind(this))
+  }
+
+  setLastRoll(event) {
+    event.persist();
+    const contract = require('truffle-contract')
+    const dice = contract(DiceContract)
+    dice.setProvider(this.state.web3.currentProvider)
+    var diceInstance;
+
+    dice.deployed().then(function(instance) {
+      diceInstance = instance;
+      return diceInstance.set(event.target.value)
+    })
   }
 
   render() {
@@ -56,10 +93,13 @@ class Dice extends React.Component {
       <div className='Dice'>
         <h1>Smart-Dice</h1>
         <p>Last Roll: { this.state.lastRoll }</p>
-        <p>House Funds: </p>
-        <p>Player Funds: </p>
+        <p>House Balance: { this.state.playerBalance }</p>
+        <p>Player Balance: { this.state.playerBalance }</p>
+        <button value="1" onClick={ this.setLastRoll }>1</button>
       </div>
     )
   }
 
 }
+
+export default Dice
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './containers/App'
+// import App from './containers/App'
+import Dice from './containers/Dice'
+
 
 ReactDOM.render(
-  <App />,
+  <Dice />,
   document.getElementById('root')
 );
